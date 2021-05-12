85eb5b8d8f57d8206dc5cfcdbc7cb20afae32ffd
ChachyDev <shalomadecoolboy@outlook.com>
2021-05-12 21:13:10 +0100

    Implement another test mod

--- client/src/main/java/org/bookmc/mod/ModTest.java
+++ client/src/main/java/org/bookmc/mod/ModTest.java
@@ -4,4 +4,8 @@ public class ModTest {
     public void start() {
         System.out.println("Thanks for noticing me!");
     }
+
+    public void anotherStart() {
+        System.out.println("Maybe I want this to be separate for cool reasons?");
+    }
 }
