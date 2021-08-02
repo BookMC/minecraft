71a38e44717a8e4cea0911c8e939ca6c3e14c60c
ChachyDev <shalomadecoolboy@outlook.com>
2021-08-02 16:26:39 +0100

    Prepare changes for the new mod classloader system.
    The actual change consists of just referencing
    BookLauncherBase instead of BookModLoader as
    it now no longer exists but references to Book
    LauncherBase will most likely be moved to BookLoader.

--- client/src/main/java/org/bookmc/mixin/internal/launch/MixinMinecraft.java
+++ client/src/main/java/org/bookmc/mixin/internal/launch/MixinMinecraft.java
@@ -2,6 +2,7 @@ package org.bookmc.mixin.internal.launch;
 
 import net.minecraft.client.Minecraft;
 import org.bookmc.client.MinecraftClient;
+import org.bookmc.common.MinecraftCommon;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,9 +10,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        MinecraftCommon.INSTANCE = new MinecraftClient();
+    }
+
     @Inject(method = "startGame", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/client/Minecraft;checkGLError(Ljava/lang/String;)V", args = "ldc=Pre startup", shift = At.Shift.AFTER))
     private void startGameHead(CallbackInfo ci) {
-        MinecraftClient.INSTANCE.preload();
+        MinecraftCommon.INSTANCE.preload();
     }
 
     @Inject(method = "startGame", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/client/Minecraft;checkGLError(Ljava/lang/String;)V", args = "ldc=Startup", shift = At.Shift.AFTER))
