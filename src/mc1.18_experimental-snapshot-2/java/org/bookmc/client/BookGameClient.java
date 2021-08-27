76eb769a0f539ea9151a04616fb94f8ebe562469
ChachyDev <shalomadecoolboy@outlook.com>
2021-08-27 17:49:27 +0100

    Add support for 1.18 experimental snapshots

---
+++ src/mc1.18_experimental-snapshot-2/java/org/bookmc/client/BookGameClient.java
@@ -0,0 +1,10 @@
+package org.bookmc.client;
+
+import org.bookmc.common.MinecraftCommon;
+
+public class BookGameClient extends MinecraftCommon {
+    @Override
+    public void load() {
+        super.load();
+    }
+}
