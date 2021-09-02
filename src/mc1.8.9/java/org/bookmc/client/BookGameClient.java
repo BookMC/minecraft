217d6ce0e7039816fdc87b4df33d081f4ca85309
ChachyDev <shalomadecoolboy@outlook.com>
2021-09-02 10:55:58 +0100

    Implement support for 1.8.9
    Didn't really want this only added it so I can mess with stuff like optifine

---
+++ src/mc1.8.9/java/org/bookmc/client/BookGameClient.java
@@ -0,0 +1,13 @@
+package org.bookmc.client;
+
+import org.bookmc.common.MinecraftCommon;
+import org.bookmc.constants.Constants;
+import org.lwjgl.opengl.Display;
+
+public class BookGameClient extends MinecraftCommon {
+    @Override
+    public void load() {
+        Display.setTitle(Display.getTitle().concat(" (" + Constants.GAME_BRANDING + ")"));
+        super.load();
+    }
+}
